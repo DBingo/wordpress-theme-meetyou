97c77f2541f48c0c422c502ea7a0fc32e8817e79
daibingquan <daibingquan@xiaoyouzi.com>
2018-03-26 09:47:07 +0800

    首页增加text类型grid, title样式修改

--- MeetYou/index.php
+++ MeetYou/index.php
@@ -10,6 +10,9 @@
             // get link and title
             $the_link = get_permalink();
             $the_title = get_the_title();
+            $categories = get_the_category();
+            $first_cate = $categories[0]->cat_name;
+            $excerpt = get_the_excerpt();
 
             // get featured images array
             if( class_exists('Dynamic_Featured_Image') ) {
@@ -33,7 +36,9 @@
                 $post_array[$post_count] = "<div class='grid square'>
                                 <a target='_blank' href='$the_link'>
                                     <img src='". $featured_images[1][full] ."'>
-                                    <div class='title'>$the_title</div>
+                                    <div class='titlecontainer'>
+                                        <div class='title'>$the_title</div>
+                                    </div>
                                 </a>
                             </div>";
                 break;
@@ -44,19 +49,22 @@
                 $post_array[$post_count] = "<div class='grid long'>
                                 <a target='_blank' href='$the_link'>
                                     <img src='". $featured_images[2][full] ."'>
-                                    <div class='title whitecolor'>$the_title</div>
+                                    <div class='titlecontainer'>
+                                        <div class='title'>$the_title</div>
+                                    </div>
                                 </a>
                             </div>";
                 break;
 
                 case 4:
                 case 8:
-                $post_array[$post_count] = "<div class='grid square'>
-                                <a target='_blank' href='$the_link'>
-                                    <img src='". $featured_images[3][full] ."'>
-                                    <div class='title'>$the_title</div>
-                                </a>
-                            </div>";
+                $post_array[$post_count] = "<div class='grid text'>
+                                                <a target='_blank' href='$the_link'>
+                                                    <div class='tag'>$first_cate</div>
+                                                    <div class='title'>$the_title</div>
+                                                    <div class='excerpt'>$excerpt</div>
+                                                </a>
+                                            </div>";
                 break;
                 default:
                 break;
@@ -70,7 +78,9 @@
             $post_array[7] = '<div class="grid long">
                                     <a target="_blank" href="https://youbaobao.meiyou.com/">
                                         <img src="http://ued.meiyou.com/wp-content/uploads/2018/03/youbaobao.png">
-                                        <div class="title">有宝宝就用柚宝宝</div>
+                                        <div class="titlecontainer">
+                                            <div class="title">有宝宝就用柚宝宝</div>
+                                        </div>
                                     </a>
                                 </div>';
         }
@@ -78,7 +88,6 @@
             $post_array[8] = '<div class="grid square">
                                     <a target="_blank" href="https://www.meiyou.com/">
                                         <img src="http://ued.meiyou.com/wp-content/themes/MeetYou/assets/9.jpg">
-                                        <div class="title whitecolor"></div>
                                     </a>
                                 </div>';
         }
@@ -86,7 +95,9 @@
             $post_array[9] = '<div class="grid square">
                                     <a target="_blank" href="https://uedkit.meiyou.com/">
                                         <img src="http://ued.meiyou.com/wp-content/themes/MeetYou/assets/ued.jpg">
-                                        <div class="title">Nice To MeetYou</div>
+                                        <div class="titlecontainer">
+                                            <div class="title">Nice To MeetYou</div>
+                                        </div>
                                     </a>
                                 </div>';
         }
